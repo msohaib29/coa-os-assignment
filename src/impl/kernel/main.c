aa0a4e3f9f8045469d79df0de0f797d2d578a875
msohaib29 <70723900+msohaib29@users.noreply.github.com>
2021-04-20 01:08:03 +0500

    Add files via upload

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,27 @@
+#include "print.h"
+
+void kernel_main(){
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW , PRINT_COLOR_BLACK);
+    
+    print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@ \n");
+    print_str("@          @          @          @          @        \n");
+    print_str("@          @          @          @          @        \n");
+    print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @          @@@@@@@@ \n");
+    print_str("       @   @          @          @                 @ \n");
+    print_str("       @   @          @          @                 @ \n");
+    print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@ \n");
+
+    print_str("\n \n \n");
+
+
+    print_str("@      @   @      @   @@@@@@@@   @@@@@@@@    \n");
+    print_str("@@     @   @      @   @              @       \n");
+    print_str("@ @    @   @      @   @              @       \n");
+    print_str("@  @   @   @      @   @@@@@@@@       @       \n");
+    print_str("@   @  @   @      @          @       @       \n");
+    print_str("@    @ @   @      @          @       @       \n");
+    print_str("@      @   @@@@@@@@   @@@@@@@@       @       \n");
+
+
+}
