54ab42eb333f3c3357d5a78160daac57f14cfe13
msohaib29 <70723900+msohaib29@users.noreply.github.com>
2021-04-20 23:09:25 +0500

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,18 +1,19 @@
 #include "print.h"
 
-void kernel_main(){
+void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW , PRINT_COLOR_BLACK);
-    
-    print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@ \n");
-    print_str("@          @          @          @          @        \n");
-    print_str("@          @          @          @          @        \n");
-    print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @          @@@@@@@@ \n");
-    print_str("       @   @          @          @                 @ \n");
-    print_str("       @   @          @          @                 @ \n");
-    print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@ \n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
 
-    print_str("\n \n \n");
+
+    // print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@ \n");
+    // print_str("@          @          @          @          @        \n");
+    // print_str("@          @          @          @          @        \n");
+    // print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @          @@@@@@@@ \n");
+    // print_str("       @   @          @          @                 @ \n");
+    // print_str("       @   @          @          @                 @ \n");
+    // print_str("@@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@   @@@@@@@@ \n");
+
+    // print_str("\n \n \n");
 
 
     print_str("@      @   @      @   @@@@@@@@   @@@@@@@@    \n");
@@ -22,6 +23,26 @@ void kernel_main(){
     print_str("@   @  @   @      @          @       @       \n");
     print_str("@    @ @   @      @          @       @       \n");
     print_str("@      @   @@@@@@@@   @@@@@@@@       @       \n");
+
+
+    print_str("\n");
+
+print_str("                        ``.//: /+ :/+...`                     \n");
+print_str("                    `-``:/+y.s yy o.s+/-`.-`                  \n");
+print_str("                   ``.+o:-/oss.hh-oso/-:o+```                 \n");
+print_str("                   /-:.:yyo/+o+hh+++/ohy:-/-+                 \n");
+print_str("                  .os:o+:ohhs:://::shho:oo:so-`               \n");
+print_str("                 -::/++ss++o./syys/.+//oo+/:--.               \n");
+print_str("                  `.-:/++oo`-ssssss: +++//::-.`               \n");
+print_str("  .....      ......`---...`   `.....`  `.-::-.`  .............\n");
+print_str("  ../hy/`    `.oy-.`.-hh:.`   `./h:.` :s/-.-/y/  y+:::oho:::+y\n");
+print_str("    `h/yy:     +s     hh`       -h   `hy`    :: `o    +h/   `o\n");
+print_str("    .h``+hs.   /s     hh`       -h    +yyo/-`         +h/     \n");
+print_str("    .h`  .oh+` /s     hh`       -h     `-+syyo-       +h/     \n");
+print_str("    .h`    -yy//o     hh.       :y    .    `:hh.      +h/     \n");
+print_str("    -h.     `/hhs     oho`     .y/   .h-    `yh`      oh/     \n");
+print_str("  .:+s+:.     `os      :oysoooo+-    .syo+++o/`     :/oso/-  ");
+
 
 
 }
